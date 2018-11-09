8fb6cb32ea109ecb762443eecf182e011c4439b1
nktnl89 <nktnl89@gmail.com>
2018-11-09 10:53:49 +0400

    gitignore и comment

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello cruel World!");
-        //System.out.println("Goodbye cruel World!");
+        ////System.out.println("Goodbye cruel World!");
     }
 }
